13af8117b09e83bd7a9592c05a85885a116b33bd
VanilaC <khunsorawit@gmail.com>
2023-07-23 14:20:49 +0700

    yolo

--- cardboar_box/linear.py
+++ cardboar_box/linear.py
@@ -7,12 +7,13 @@ class linear:
         self.bias = np.zeros((1, x))
     
     def forward(self, inputs):
+        self.inputs = inputs
         nobias = np.matmul(inputs, self.weight)
         output = nobias + self.bias
         return output
     
-    def backward(self, inputs, x, lr=0.1):
-        dw = np.matmul(inputs.T, x)
+    def backward(self,  x, lr=1):
+        dw = np.matmul(self.inputs.T, x)
         db = np.sum(x, axis=0)
         self.weight -= dw * lr
         self.bias -= db * lr
@@ -28,3 +29,35 @@ class linear:
         data = np.load(filename)
         self.weight = data["weight"]
         self.bias = data["bias"]
+
+def softmax(x, derivative=False):
+    if not derivative:
+        exp_x = np.exp(x)
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    else:
+        return softmax(x) * (1 - softmax(x).sum(axis=1, keepdims=True))
+
+def relu(x,derivative = False):
+    if not derivative:
+        return np.maximum(0, x)
+    else:
+        return (x > 0).astype(float)
+
+ins = np.array([[0.1,1,1]])
+target = np.array([0,0,1])
+
+layerone = linear(3,5)
+layertwo = linear(5,3)
+for s in range(20):
+    x = relu(layerone.forward(ins))
+    x = softmax(layertwo.forward(x))
+
+    error = x - target 
+
+    layertwo.backward(error)
+    y = layertwo.deri(error) *  relu(layerone.forward(ins),True)
+    layerone.backward(y)
+
+    print(error)
+
+print(x)
